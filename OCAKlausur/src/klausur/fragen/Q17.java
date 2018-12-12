832e94a5675a6f175ea744404ea807d407f32c57
mamiri <mamiri@PC05160760.cc-college.gecs.loc>
2018-12-12 08:22:00 +0100

    versuch1

---
+++ OCAKlausur/src/klausur/fragen/Q17.java
@@ -0,0 +1,47 @@
+package klausur.fragen;
+
+
+import sun.applet.Main;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mamiri
+ */
+public class Q17 {
+    
+    public int amount;
+    
+    public Q17( int amount){
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+    
+    public void changeAmount(int x ) {
+        amount += x;
+    }
+    
+    public static void main(String[] args) {
+        Q17 acct = new Q17((int)(Math.random() * 1000));
+        
+        //this.amount = 0;
+        //amount = 0;
+        //acct(0);
+     acct.amount = 0;
+        //acct.getAmount() = 0;
+        //acct.changeAmount(0);
+    // acct.changeAmount(-acct.amount);
+    // acct.changeAmount(-acct.getAmount());
+        
+        
+        System.out.println(acct.getAmount());
+    }
+}
---
+++ OCAKlausur/src/klausur/fragen/Q25.java
@@ -0,0 +1,41 @@
+
+package klausur.fragen;
+
+
+public class Q25 {
+    String name;
+    boolean contract;
+    double salary;
+    
+    Q25() {
+//        this.name = new String("Moe");
+//        this.contract = new Boolean(true);
+//        this.salary = new Double(100);
+        
+//        name = "Jooo";
+//        contract = true;
+//        salary = 1000;
+
+//        this("Joe", true, 100.0);
+    }
+    
+    public String toString() {
+        return name + " : " + contract + " : " + salary; 
+    }
+    
+    public static void main(String[] args) {
+        Q25 q = new Q25();
+        
+//        q.name = "Joe";
+//        q.contract = true;
+//        q.salary = 100;
+
+//          this.name = "Joe";
+//          this.contract = true;
+//          this.salary = 100;
+
+            
+        
+        System.out.println(q);
+    }
+}
