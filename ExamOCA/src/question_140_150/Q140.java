547cad80eb4d510a9f0794199969889604b19f00
Moe <schaunach@gmail.com>
2019-01-12 11:07:47 +0100

    Merge origin/master
    Conflicts:
    	OCAKlausur/src/fragen/Q135.java

---
+++ ExamOCA/src/question_140_150/Q140.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package question_140_150;
+
+/**
+ *
+ * @author Moe
+ */
+public class Q140 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        int i=0;
+        do{
+            System.out.print("i: "+i + " ");
+            System.out.print("arr[i]: " + arr[i] + " ");
+            i++;
+            System.out.println("i++: " + i);
+        } while(i < arr.length - 1);
+    }
+}
+
+/**
+ *      i    arr[i]   i++     i<arr.length-1
+ *      0       1       1           2               
+ *      1       2       2           2
+ *      2       3       3   springt raus aus Bedingung 
+ */
